eeb251978059743dc8a9fef33b0bfadbdb2604f2
Elias Persson <delreich@takeit.se>
2013-09-24 18:24:33 +0200

    Add header to relevant python files
    Shebang, future imports and vim modeline.

--- caramel/models.py
+++ caramel/models.py
@@ -1,3 +1,16 @@
+#! /usr/bin/env python
+# vim: expandtab shiftwidth=4 softtabstop=4 tabstop=17 filetype=python :
+
+# Make things as three-ish as possible (requires python >= 2.6)
+from __future__ import (unicode_literals, print_function,
+                        absolute_import, division)
+# Namespace cleanup
+del unicode_literals, print_function, absolute_import, division
+
+#
+# ----- End header -----
+#
+
 from sqlalchemy import (
     Column,
     Integer,
