91ee59fce17cf430afddbe20b5e3b02903af6497
D.S. Ljungmark <ljungmark@modio.se>
2016-05-08 16:11:39 +0200

    Version 1.6 released

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ deplinks = [
     ]
 
 setup(name="caramel",
-      version="1.4.1",
+      version="1.6.0",
       description="caramel",
       long_description="""
 Caramel is a certificate management system that makes it easy to use client
