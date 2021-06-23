4d0cbb1d8947636f6d3365547953cad6862bcbc9
Teo Tiefenbacher <teo.tiefenbacher@gmail.com>
2021-06-23 10:19:48 +0200

    Scripts: Move caramel_launcher.sh to scripts
    Since caramel_launcher.sh is a sh script move it to the other scripts. So to
    not break anything the Makefile is modified to look for the script there.
    To acctually facilitate the needs of Makefile caramel_launcher takes 1 argument
    which is what pserve to use, since in the testing pserve from the virtual
    environment needs to be used.

--- scripts/caramel_launcher.sh
+++ scripts/caramel_launcher.sh
@@ -1,7 +1,15 @@
 #! /bin/sh
 ## Launch caramel with pserve as a Pyramid WSGI app using environment variables
 ## for configuration
-pserve caramel_launcher.ini \
+SCRIPT_HOME="$(dirname "$(realpath "$0")")"
+
+if [ -z "$1" ]; then
+  PSERVE=$1
+else
+  PSERVE=pserve
+fi
+
+exec $PSERVE "${SCRIPT_HOME}/caramel_launcher.ini" \
   ca_cert="${CARAMEL_CA_CERT:-${BASEDIR}/example/caramel.ca.cert}" \
   ca_key="${CARAMEL_CA_KEY:-${BASEDIR}/example/caramel.ca.key}" \
   http_port="${CARAMEL_PORT:-6543}" \
@@ -9,4 +17,4 @@ pserve caramel_launcher.ini \
   life_short="${CARAMEL_LIFE_SHORT:-48}" \
   life_long="${CARAMEL_LIFE_LONG:-720}" \
   dburl="${CARAMEL_DBURL:-sqlite:///${BASEDIR}/caramel.sqlite}" \
-  log_level="${CARAMEL_LOG_LEVEL:-INFO}"
+  log_level="${CARAMEL_LOG_LEVEL:-ERROR}"
