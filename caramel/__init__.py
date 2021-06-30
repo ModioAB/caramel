3c1357627bf1921fdee114b60f96f42c328120b4
Teo Tiefenbacher <teo.tiefenbacher@gmail.com>
2021-06-30 09:58:39 +0200

    Caramel: Move pyramid_tm include to caramel.main
    Move the setting to include pyramid_tm to caramel.main from ini files.
    This is a vital setting that should never be changed by the user.

--- caramel/__init__.py
+++ caramel/__init__.py
@@ -13,6 +13,7 @@ def main(global_config, **settings):
     engine = engine_from_config(settings, "sqlalchemy.")
     init_session(engine)
     config = Configurator(settings=settings)
+    config.include("pyramid_tm")
     config.add_route("ca", "/root.crt", request_method="GET")
     config.add_route("cabundle", "/bundle.crt", request_method="GET")
     config.add_route("csr", "/{sha256:[0-9a-f]{64}}", request_method="POST")
